a1f8354d47ff03c18c9f3535af52b725bc39ebfd
Niels Schelbach <niels.schelbach@rocketbase.io>
2016-11-01 09:20:40 +0100

    adding forwarding of rancher internal dns service. Removing trace of easys rsa setup, this should be given

--- bin/prepare-vpn.sh
+++ bin/prepare-vpn.sh
@@ -4,28 +4,6 @@
 
 set -e
 
-if [ ! -d $VPN_PATH/easy-rsa ]; then
-   # Copy easy-rsa tools to /etc/openvpn
-   rsync -avz /usr/share/easy-rsa $VPN_PATH/
-
-   # Configure easy-rsa vars file
-   perl -p -i -e "s/export KEY_COUNTRY=.*/export KEY_COUNTRY=\"CA\"/g" $VPN_PATH/easy-rsa/vars
-   perl -p -i -e "s/export KEY_PROVINCE=.*/export KEY_PROVINCE=\"BARCELONA\"/g" $VPN_PATH/easy-rsa/vars
-   perl -p -i -e "s/export KEY_CITY=.*/export KEY_CITY=\"CASTELLDEFELS\"/g" $VPN_PATH/easy-rsa/vars
-   perl -p -i -e "s/export KEY_ORG=.*/export KEY_ORG=\"NIXEL\"/g" $VPN_PATH/easy-rsa/vars
-   perl -p -i -e "s/export KEY_EMAIL=.*/export KEY_EMAIL=\"manel\@nixelsolutions.com\"/g" $VPN_PATH/easy-rsa/vars
-   perl -p -i -e "s/export KEY_OU=.*/export KEY_OU=\"NIXEL\"/g" $VPN_PATH/easy-rsa/vars
-
-   pushd $VPN_PATH/easy-rsa
-   . ./vars
-   ./clean-all
-   ./build-ca --batch
-   ./build-key-server --batch server
-   ./build-dh
-   ./build-key --batch RancherVPNClient
-   openvpn --genkey --secret keys/ta.key
-   popd
-fi
 
 # Update openvpn route
 #RANCHER_NETWORK_CIDR=`ip addr show dev eth0 | grep inet | grep 10.42 | awk '{print $2}' | xargs -i ipcalc -n {} | grep Network | awk '{print $2}' | awk -F/ '{print $1}'`
@@ -58,6 +36,8 @@ server 10.8.0.0 255.255.255.0
 duplicate-cn
 
 push "route ${ROUTED_NETWORK_CIDR} ${ROUTED_NETWORK_MASK}"
+push "route 169.254.169.250 255.255.255.255"
+push "dhcp-option DNS 169.254.169.250" 
 EOF
 
 # Enable tcp forwarding and add iptables MASQUERADE rule
